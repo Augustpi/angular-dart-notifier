47c98f6bf13553ba2f81e43a1567b641b04039b4
Avni Onur Pehlivan <onur@woorklab.com>
2018-08-18 20:57:22 +0300

    First commit for angular dart web notifier
    angular dart web notifier

---
+++ lib/app_component.dart
@@ -0,0 +1,18 @@
+import 'package:angular/angular.dart';
+import 'package:notifier/src/notifier.dart';
+
+@Component(
+  selector: 'my-app',
+  styleUrls: ['app_component.css'],
+  templateUrl: 'app_component.html',
+  directives: [
+    coreDirectives,
+  ],
+)
+class AppComponent {
+
+  void sendNotifier(String type, String title, String content, int duration){
+    new Notifier(type, title, content, duration).show();
+  }
+
+}
---
+++ lib/src/notifier.dart
@@ -0,0 +1,81 @@
+import 'dart:async';
+import 'dart:html';
+
+class Notifier {
+
+  String type;
+  String title;
+  String content;
+  Element container;
+  int duration;
+  int counter;
+  
+  Notifier(this.type, this.title, this.content, this.duration);
+
+  show() {
+    container = createContainer();
+    createNotification(type, title, content, container, duration);
+  }
+
+  createContainer() {
+    print(querySelector('#notifier-container') == null);
+    if (querySelector('#notifier-container') != null) {
+      counter = querySelector('#notifier-container').children.length;
+      print(counter);
+      counter += 1;
+      print(counter);
+      return querySelector('#notifier-container');    
+    } else {
+      counter = 0;
+      var element = _creteElement('div', {'id':'notifier-container', 'class':'notify container'});
+      document.body.append(element);
+      return querySelector('#notifier-container');
+    }
+  }
+
+  createNotification(String type, String title, String content, Element container, int duration) {
+    var itemId = 'notifier-item-'+ counter.toString();
+    var itemEl = _creteElement('div', {'class':'notify item ' + type, 'id' : itemId});
+    var titleEl = _creteElement('div', {'class': 'header'});
+    var contentEl = _creteElement('div', {'class': 'content'});
+    var clsEl = _creteElement('div', {'class': 'close-btn'});
+    var iconEl = _creteElement('div', {'class': 'img img-' + type});
+
+    titleEl.innerHtml = title;
+    contentEl.innerHtml = content;
+    clsEl.innerHtml = 'x';
+    
+    clsEl.onClick.listen((e) {
+        closeNotification(itemId);
+    });
+
+    itemEl.append(clsEl);
+    itemEl.append(titleEl);
+    itemEl.append(iconEl);
+    itemEl.append(contentEl);
+    container.append(itemEl);
+    
+    Timer(
+      Duration(milliseconds: 100), (){
+      itemEl.classes.add('show-notifier');
+    });
+
+    if (duration>0) {
+      Timer(Duration(
+        milliseconds: duration), (){
+        closeNotification(itemId);
+      });
+    }
+  }
+
+  void closeNotification(String currentEl){
+    querySelector('#'+currentEl).classes.remove('show-notifier');
+    Timer(Duration(milliseconds: 600), querySelector('#'+currentEl).remove);
+  }
+
+  Element _creteElement(String el, Map<String, String> attrs) {
+    var element = document.createElement(el);
+    attrs.forEach((name, value) => element.setAttribute(name, value));
+    return element;
+  }
+}
